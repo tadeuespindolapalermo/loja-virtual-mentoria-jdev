1910cfdc4b515c2b3c556ae7d245ae7d9cee3dae
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2022-05-31 00:54:27 -0300

    tabela categoria_produto

--- src/main/java/br/com/tadeudeveloper/lojavirtual/model/CategoriaProduto.java
+++ src/main/java/br/com/tadeudeveloper/lojavirtual/model/CategoriaProduto.java
@@ -12,17 +12,17 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable {
 
-	private static final long serialVersionUID = 5538536958503261566L;
+	private static final long serialVersionUID = 487468306028298255L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 
-	@Column(nullable = false)
+	@Column(name = "nome_desc", nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
@@ -54,7 +54,7 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		return Objects.equals(id, other.id);
 	}
 
