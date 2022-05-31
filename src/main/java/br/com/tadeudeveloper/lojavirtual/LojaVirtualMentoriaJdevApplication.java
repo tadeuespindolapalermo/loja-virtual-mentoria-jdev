4756a24367457a32c84e168e6575a9ad882d8db4
Tadeu Espindola Palermo <tadeupalermoti@gmail.com>
2022-05-30 23:39:38 -0300

    commit initial projeto mentoria java jdev loja virtual

---
+++ src/main/java/br/com/tadeudeveloper/lojavirtual/LojaVirtualMentoriaJdevApplication.java
@@ -0,0 +1,13 @@
+package br.com.tadeudeveloper.lojavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualMentoriaJdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualMentoriaJdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tadeudeveloper/lojavirtual/LojaVirtualMentoriaJdevApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tadeudeveloper.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualMentoriaJdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
